d7443254f1fef5562f92d85078203da21b7c76eb
doublebyte1 <doublebyte@gmail.com>
2017-01-04 12:02:57 +0100

    - Removed mvc dependency - Added default application response

--- Data-Ingest/src/main/java/org/opengeoportal/DataIngestApplication.java
+++ Data-Ingest/src/main/java/org/opengeoportal/DataIngestApplication.java
@@ -1,12 +1,20 @@
 package org.opengeoportal;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class DataIngestApplication {
 
-    public static void main(String[] args) {
+    @RequestMapping("/")
+    String home() {
+        return "Data Ingest";
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(DataIngestApplication.class, args);
     }
 }
