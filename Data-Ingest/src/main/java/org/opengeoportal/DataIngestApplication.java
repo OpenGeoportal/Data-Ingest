455ae33c8209112308927c6aa5cacc807e2617c4
doublebyte1 <doublebyte@gmail.com>
2017-01-04 11:12:49 +0100

    - Created Mvn Spring Boot project with dependencies

---
+++ Data-Ingest/src/main/java/org/opengeoportal/DataIngestApplication.java
@@ -0,0 +1,12 @@
+package org.opengeoportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIngestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataIngestApplication.class, args);
+    }
+}
