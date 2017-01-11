4a24fe97f4fa1c46de160aad40359e1caa643ce9
doublebyte1 <doublebyte@gmail.com>
2017-01-11 15:06:23 +0100

    - Fixed checkstyle issues:  - Added javaDoc comments  - Added final methods and variables  - Added package-info.java  - Added newline on the end of file

--- src/main/java/org/opengeoportal/DataIngestApplication.java
+++ src/main/java/org/opengeoportal/DataIngestApplication.java
@@ -5,16 +5,42 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Creates a RESTfull servrlet application.
+ * Introduces a default context.
+ *
+ * @author Joana Simoes
+ * @version 1.0
+ * @since   2017-01-11
+ */
 @RestController
 @EnableAutoConfiguration
 public class DataIngestApplication {
 
+    /**
+     * Annotation for mapping a default web request onto specific
+     * handler method.
+     * @return Nothing
+     */
+
     @RequestMapping("/")
-    String home() {
+
+    /**
+     * This is a demonstration method which returns a string.
+     * @return String Demo string, to be displayed by the web application.
+     */
+
+    final String home() {
         return "Data Ingest";
     }
 
-    public static void main(String[] args) throws Exception {
+    /**
+     * This is the main method which runs the web application.
+     * @param args Unused.
+     * @exception Exception General exception
+     */
+
+    public static void main(final String[] args) throws Exception {
         SpringApplication.run(DataIngestApplication.class, args);
     }
 }
---
+++ src/main/java/org/opengeoportal/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Created by joana on 11/01/17.
+ *
+ * Provides the necessary classes to implement a data ingestion API.
+ *
+ * The Data Ingest component interfaces with Geoserver, allowing to list,
+ * upload, download, update and delete vector data stores and associated
+ * metadata.
+ *
+ * It provides both a REST API and a UI.
+ *
+ */
+package org.opengeoportal;
