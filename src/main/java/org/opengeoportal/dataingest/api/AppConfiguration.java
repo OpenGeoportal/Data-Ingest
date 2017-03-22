bbb915e8a4e9ec0a33ab9b1cdbe0cd9b4a474802
Pascallike <antonio.cerciello@gmail.com>
2017-03-22 17:51:00 +0100

    CORS Configurable Filter

---
+++ src/main/java/org/opengeoportal/dataingest/api/AppConfiguration.java
@@ -0,0 +1,41 @@
+package org.opengeoportal.dataingest.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * The Class AppConfiguration.
+ */
+@Configuration
+public class AppConfiguration {
+
+    /** The GUI host. */
+    @Value("${param.dataingestgui.host}")
+    private String GUIHost;
+
+    /**
+     * Cors filter.
+     *
+     * @return the filter registration bean
+     */
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(GUIHost);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(
+                new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
