4854daa9bf10b68e3afe847607c89381ec499a58
doublebyte1 <doublebyte@gmail.com>
2017-04-13 14:10:34 +0200

    - fill list datasets cache on init

--- src/main/java/org/opengeoportal/dataingest/api/CacheService.java
+++ src/main/java/org/opengeoportal/dataingest/api/CacheService.java
@@ -1,11 +1,15 @@
 package org.opengeoportal.dataingest.api;
 
 import org.opengeoportal.dataingest.exception.NoDataFoundOnGeoserverException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -20,14 +24,38 @@ import java.util.Map;
  */
 @Component
 public class CacheService {
-
     /**
-     * Get datasets typenames, workspaces, names and titles, for a given GeoServer uri.
-     *
-     * @param uri geoserver uri (it may include the filter for workspace).
-     * @return hashmap with dataset (names, titles)
-     * @throws Exception the exception
+     * Spring boot logger.
      */
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    /**
+     * The GeoServer URL (from the application.properties).
+     */
+    @Value("${geoserver.url}")
+    private String geoserverUrl;
+    /**
+     * Fills cache on startup.
+     *
+     * @throws Exception
+     */
+    @PostConstruct
+    private void fillCache() throws Exception {
+        try {
+            log.info("Filling datasets list cache...");
+            getDatasets(geoserverUrl);
+            log.info("Cached filled.");
+        } catch (Exception e) {
+            throw new Exception("Could not initialize datasets list cache!");
+        }
+    }
+
+        /**
+         * Get datasets typenames, workspaces, names and titles, for a given GeoServer uri.
+         *
+         * @param uri geoserver uri (it may include the filter for workspace).
+         * @return hashmap with dataset (names, titles)
+         * @throws Exception the exception
+         */
     @Cacheable(value = "titles", key = "#uri")
     public List<Map<String, String>> getDatasets(final String uri)
         throws Exception {
