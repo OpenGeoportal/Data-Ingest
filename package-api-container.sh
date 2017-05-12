0703a8a66b5618deddc88a66af63cb3be5dff4bd
doublebyte1 <doublebyte@gmail.com>
2017-05-12 18:27:17 +0200

    - Give access to solr to the data ingest container.

--- package-api-container.sh
+++ package-api-container.sh
@@ -3,9 +3,15 @@
 sed -ie 's#geoserver.url=http://localhost:8081/geoserver/#geoserver.url=http://gs:8080/geoserver/#g' \
  src/main/resources/application.properties;
 
+sed -ie 's#localSolr.url=http://localhost:8983/solr/ogp#localSolr.url=http://solr:8983/solr/ogp#g' \
+ src/main/resources/application.properties
+
 mvn package -DskipTests -Drelax && cp target/Data-Ingest-0.0.1-SNAPSHOT.jar docker/data-ingest/;
 
 sed -ie 's#geoserver.url=http://gs:8080/geoserver/#geoserver.url=http://localhost:8081/geoserver/#g' \
+ src/main/resources/application.properties
+
+sed -ie 's#localSolr.url=http://solr:8983/solr/ogp#localSolr.url=http://localhost:8983/solr/ogp#g' \
  src/main/resources/application.properties
 
 echo "ok";
