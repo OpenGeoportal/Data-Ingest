27b25cb1bf2f4a55b71ba9ff6f158b4d2466333b
doublebyte1 <doublebyte@gmail.com>
2017-10-05 10:12:20 +0200

    - make data-ingest container wait for geoserver - fixed package-api-container script

--- package-api-container.sh
+++ package-api-container.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-sed -ie 's#geoserver.url=http://localhost:8081/geoserver/#geoserver.url=http://gs:8080/geoserver/#g' \
+sed -i 's#http://localhost:8081/geoserver/#http://gs:8080/geoserver/#g' \
  src/main/resources/application.properties;
 
-sed -ie 's#localSolr.url=http://localhost:8983/solr/ogp#localSolr.url=http://solr:8983/solr/ogp#g' \
+sed -i 's#http://localhost:8983/solr/ogp#http://solr:8983/solr/ogp#g' \
+ src/main/resources/application.properties
+
+sed -i 's#http://localhost:8080#http://tomcat:8082#g' \
  src/main/resources/application.properties
 
 mvn package -DskipTests -Drelax && cp target/Data-Ingest-0.0.1-SNAPSHOT.jar docker/data-ingest/;
 
-sed -ie 's#geoserver.url=http://gs:8080/geoserver/#geoserver.url=http://localhost:8081/geoserver/#g' \
+sed -i 's#http://gs:8080/geoserver/#http://localhost:8081/geoserver/#g' \
  src/main/resources/application.properties
 
-sed -ie 's#localSolr.url=http://solr:8983/solr/ogp#localSolr.url=http://localhost:8983/solr/ogp#g' \
+sed -i 's#http://solr:8983/solr/ogp#http://localhost:8983/solr/ogp#g' \
+ src/main/resources/application.properties
+
+sed -i 's#http://tomcat:8082#http://localhost:8080#g' \
  src/main/resources/application.properties
 
 echo "ok";
